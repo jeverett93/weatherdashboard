46b33a533995f7b735b9455d96aef181df2eb005
Josh Everett <josheverett93@gmail.com>
2020-03-18 13:35:03 -0500

    corrected CSS link and UV index color working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 
 //moment
 var momentTime = moment().format("MMMM Do YYYY");
-console.log(momentTime);
+
 
 //variables
 var APIKey = "e885fd3db621744dfef49f4c1e174dc6";
@@ -11,7 +11,7 @@ var currentWeather = $("#current-weather");
 var fiveDay = $("#five-day");
 var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
-
+// function for page to hold search history
 function loadHistory() {
     for (var i = 0; i < searchHistory.length; i++) {
         var historyDivs = $('<div>');
@@ -22,6 +22,7 @@ function loadHistory() {
     }
 }
 
+// function for 5 day forecast 
 function getForecast (city){
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5&appid=" + APIKey + "&units=imperial"
     console.log(queryURL)
@@ -36,18 +37,29 @@ function getForecast (city){
             for (var j = 0; j < 5; j++){
                 var farenTemp = Math.floor((forecastData.list[j].main.temp));
                 var fiveDayDate = moment().add(j + 1, "day").format("MMMM Do YYYY");
-                var newerDiv = $("<div>")
-                // fiveDay.empty();
-                $('<div>').text("Date: " + fiveDayDate).appendTo(fiveDay);
-                $('<div>').text("Current Temperature (F): " + farenTemp).appendTo(fiveDay);
-                $('<div>').text("Humidity: " + forecastData.list[j].main.humidity + "%").appendTo(fiveDay);
+                
+                var card = $('<div class ="card">');
+                var cardBody = $('<div class="card-body">');
+                var cardIcon = $('<img class="images" src ="https://openweathermap.org/img/wn/' + forecastData.list[j].weather[0].icon + '@2x.png"/>')
+                cardIcon.appendTo(cardBody);
+
+                fiveDay.empty();
+                $('<p class="card-text">').text(fiveDayDate).appendTo(cardBody);
+                $('<p class="card-text">').text("Temperature (F): " + farenTemp).appendTo(cardBody);
+                $('<p class="card-text">').text("Humidity: " + forecastData.list[j].main.humidity + " %").appendTo(cardBody);
+
+            
+            card.append(cardBody);
+
+            fiveDay.append(card);
+
             }
             
             
         })
 };
 
-
+// function to get the current weather
 function getCurrentWeather(city) {
     var cityInput = city || $('#cityInput').val().trim();
 
@@ -62,8 +74,11 @@ $.ajax({
     .then(function(weatherData) {
         
         console.log(weatherData);
+        // converting temperatures from Kelvin to Fahrenheit 
         var farenTemp = Math.floor((weatherData.main.temp - 273.15) * 1.8 + 32);
         var feelsLike = Math.floor((weatherData.main.feels_like - 273.15) * 1.8 + 32);
+
+        // adding icons to weather condition
         var imgIcon = $('<img>');
         imgIcon.attr('class', 'image');
         imgIcon.attr('src', 'https://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png');
@@ -79,6 +94,7 @@ $.ajax({
         $('<h3>').text("Humidity: " + weatherData.main.humidity + "%").appendTo(currentWeather)
         $('<h3>').text("Wind Speed: " + weatherData.wind.speed + " mph").appendTo(currentWeather)
 
+        // variables holding latitude and longitude 
         var lat = weatherData.coord.lat;
         var lon = weatherData.coord.lon;
 
@@ -88,7 +104,7 @@ $.ajax({
             url: queryURL2,
             method: "GET"
           })
-            // We store all of the retrieved data inside of an object called "response"
+            // Getting UV index and color code for index
             .then(function(moreData) {
                 console.log(moreData);
                 $('<h3 id = ' + city + '>').text("UV Index: " + moreData.value).appendTo(currentWeather);
@@ -137,8 +153,10 @@ submitCity.on("click", function(event) {
 
     cityHistory.prepend(newDiv);
 
-    $('#cityInput').val("");
+    
     getCurrentWeather();
+
+    $('#cityInput').val("");
 });
 
 $(document).on("click", ".saved-city", function(){
